c18d6d8386b1210d7557ea83bdf8f48961057f17
Francisco Farrando <121203370+franciscofarrando@users.noreply.github.com>
2025-04-21 16:58:30 +0200

    error ID arreglado

--- src/main/java/com/example/demo/controllers/Controller.java
+++ src/main/java/com/example/demo/controllers/Controller.java
@@ -1,4 +1,26 @@
-package controllers;
+package com.example.demo.controllers;
+
+import com.example.demo.models.Person;
+import com.example.demo.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 
 public class Controller {
+    @Autowired
+    private PersonRepository personRepository;
+    @GetMapping("/person")
+    /* public List<Person> findAll(){
+        return personRepository.findAll();
+    }
+    */
+
+   public String save(){
+        return "hola";
+    }
+
 }
